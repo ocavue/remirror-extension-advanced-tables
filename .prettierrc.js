46ab24f293778f830092f5d53e094f4a77215f19
ocavue <ocavue@gmail.com>
2021-01-31 16:45:09 +0800

    chore: add formater and linter

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/** @type {import('prettier').Options} */
+const config = {
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 140,
+  proseWrap: 'never',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
+
+module.exports = config;
