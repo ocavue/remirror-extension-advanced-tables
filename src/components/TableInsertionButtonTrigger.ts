b28f8b48cd3f73218eb269433231640910bbd58e
ocavue <ocavue@gmail.com>
2021-02-11 22:12:49 +0800

    chore: remove debug background

--- src/components/TableInsertionButtonTrigger.ts
+++ src/components/TableInsertionButtonTrigger.ts
@@ -57,7 +57,7 @@ const TriggerArea = ({
     zIndex: 12,
 
     // Just for debug. Use linear-gradient as background so that we can differentiate two neighbor areas.
-    background: 'linear-gradient(to left top, rgba(0, 255, 100, 0.3), rgba(200, 100, 255, 0.3))',
+    // background: 'linear-gradient(to left top, rgba(0, 255, 100, 0.3), rgba(200, 100, 255, 0.3))',
   };
 
   const showButton = () => {
