3e3333bf11e65a336585c734d096a0d1287fcb35
ocavue <ocavue@gmail.com>
2021-02-18 17:56:24 +0800

    feat: add more table sizes in the demo

--- example/Menu.tsx
+++ example/Menu.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 interface MenuProps {
-  createTable: () => void;
+  createTable: (params: { columnsCount: number; rowsCount: number; withHeaderRow?: boolean }) => void;
 }
 
 const Menu: React.FC<MenuProps> = ({ createTable }) => {
   return (
     <div>
-      <button onClick={createTable}>insert a table</button>
+      <button onClick={() => createTable({ rowsCount: 3, columnsCount: 3 })}>insert a 3*3 table</button>
+      <button onClick={() => createTable({ rowsCount: 5, columnsCount: 5 })}>insert a 5*5 table</button>
+      <button onClick={() => createTable({ rowsCount: 8, columnsCount: 100 })}>insert a 8*100 table</button>
     </div>
   );
 };
