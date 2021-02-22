0dcf56d98a0bbc249a9d074965ebfc76340ac9f1
ocavue <ocavue@gmail.com>
2021-02-22 21:58:50 +0800

    fix: update button testid value

--- example/Menu.tsx
+++ example/Menu.tsx
@@ -7,13 +7,13 @@ interface MenuProps {
 const Menu: React.FC<MenuProps> = ({ createTable }) => {
   return (
     <div>
-      <button data-testid='btn-a' onClick={() => createTable({ rowsCount: 3, columnsCount: 3, withHeaderRow: false })}>
+      <button data-testid='btn-3-3' onClick={() => createTable({ rowsCount: 3, columnsCount: 3, withHeaderRow: false })}>
         insert a 3*3 table
       </button>
-      <button data-testid='btn-b' onClick={() => createTable({ rowsCount: 5, columnsCount: 5, withHeaderRow: false })}>
+      <button data-testid='btn-5-5' onClick={() => createTable({ rowsCount: 5, columnsCount: 5, withHeaderRow: false })}>
         insert a 5*5 table
       </button>
-      <button data-testid='btn-c' onClick={() => createTable({ rowsCount: 100, columnsCount: 8, withHeaderRow: false })}>
+      <button data-testid='btn-8-100' onClick={() => createTable({ rowsCount: 100, columnsCount: 8, withHeaderRow: false })}>
         insert a 8*100 table
       </button>
     </div>
--- src/test/e2e.spec.ts
+++ src/test/e2e.spec.ts
@@ -8,17 +8,17 @@ async function checkEditorScreenshot() {
 describe('Smoke test', function () {
   test('3*3', async () => {
     await page.goto('http://localhost:1234', { timeout: 30 * 1000 });
-    await page.click('[data-testid=btn-a]');
+    await page.click('[data-testid=btn-3-3]');
     await checkEditorScreenshot();
   });
   test('5*5', async () => {
     await page.goto('http://localhost:1234', { timeout: 30 * 1000 });
-    await page.click('[data-testid=btn-b]');
+    await page.click('[data-testid=btn-5-5]');
     await checkEditorScreenshot();
   });
   test('8*100', async () => {
     await page.goto('http://localhost:1234', { timeout: 30 * 1000 });
-    await page.click('[data-testid=btn-c]');
+    await page.click('[data-testid=btn-8-100]');
     await checkEditorScreenshot();
   });
 });
